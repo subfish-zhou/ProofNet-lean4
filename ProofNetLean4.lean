eb972d04d0c437540a5609b95991ebc2c7413de5
subfish-zhou <infinitylyceum@163.com>
2024-07-15 19:01:56 +0800

    minor fix

--- ProofNetLean4.lean
+++ ProofNetLean4.lean
@@ -1,9 +1,9 @@
 import «ProofNetLean4».Artin
 import «ProofNetLean4».Axler
-import «ProofNetLean4».Cambridge-Tripos
-import «ProofNetLean4».Dummit-Foote
+import «ProofNetLean4».«Cambridge-Tripos»
+import «ProofNetLean4».«Dummit-Foote»
 import «ProofNetLean4».Herstein
-import «ProofNetLean4».Ireland-Rosen
+import «ProofNetLean4».«Ireland-Rosen»
 import «ProofNetLean4».Munkers
 import «ProofNetLean4».Pough
 import «ProofNetLean4».Putnam
